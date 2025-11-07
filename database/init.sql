3aeea7427038271f6fc9c1716edc5ca8d3092757
Siddharth Balaji <siddharthbalaji6@gmail.com>
2025-11-06 19:54:25 -0800

    feat: added indexes to the init.sql

--- database/init.sql
+++ database/init.sql
@@ -16,15 +16,17 @@ CREATE TABLE spans (
     start_time DATETIME,
     end_time DATETIME,
     duration INTERVAL NOT NULL,
+    -- the input and output preview are just the first 200 characters of the input and output for quick viewing when displaying on the frontend DAG--
     input_preview VARCHAR(200),
+    -- the input and output blob urls are used for storing the url at which the data is stored--
     input_blob_url VARCHAR(250),
     output_preview VARCHAR(200),
     output_blob_url VARCHAR(250),
-    status VARCHAR(50),
     llm_model VARCHAR(50),
     prompt_tokens INT,
     completion_tokens INT,
     cost FLOAT,
+    status VARCHAR(50),
     error_message VARCHAR(200),
 );
 
@@ -43,3 +45,11 @@ CREATE TABLE evaluations (
     evaluator_llm VARCHAR(200),
     score INT
 );
+
+
+CREATE INDEX idx_traces_user_id ON traces(user_id);
+CREATE INDEX idx_spans_trace_id ON spans(trace_id);
+CREATE INDEX idx_spans_parent_span_id ON spans(parent_span_id);
+CREATE INDEX idx_spans_status ON spans(status);
+CREATE INDEX idx_prompt_versions_version_number ON prompt_versions(version_number);
+CREATE INDEX idx_evaluations_trace_id ON evaluations(trace_id);
