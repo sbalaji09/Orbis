b43a95624fc66f404679eca6d49d24fb13645531
Rahul Thennarasu <rahulthennarasu07@gmail.com>
2025-11-19 16:36:20 -0800

    feat: complete SDK with LLM provider integrations

--- observability_sdk/__init__.py
+++ observability_sdk/__init__.py
@@ -8,5 +8,14 @@ from .core.span import Span
 from .decorators.observe import observe
 from .collector.config import configure, get_config
 from .collector.collector import get_collector
+from .integrations import anthropic_integration, openai_integration
 
-__all__ = ["Span", "observe", "configure", "get_config", "get_collector"]
+__all__ = [
+    "Span", 
+    "observe", 
+    "configure", 
+    "get_config", 
+    "get_collector",
+    "openai_integration",
+    "anthropic_integration", 
+]
