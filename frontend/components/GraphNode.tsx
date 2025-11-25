c07510284895b00e5a215c99338244c170a7c61b
Siddharth Balaji <siddharthbalaji6@gmail.com>
2025-11-25 01:28:04 -0800

    feat: working on implementing streaming status in  node

--- frontend/components/GraphNode.tsx
+++ frontend/components/GraphNode.tsx
@@ -618,6 +618,23 @@ export default function GraphNode({
                       </div>
                     )}
 
+                    {span.is_streaming && (
+                      <div className="flex gap-4">
+                        {span.time_to_first_token && (
+                          <div>
+                            <span className="text-black/60">TTFT:</span>
+                            <span className="font-mono">{span.time_to_first_token.toFixed(0)}ms</span>
+                          </div>
+                        )}
+                        {span.tokens_per_second && (
+                          <div>
+                            <span className="text-black/60">Speed:</span>
+                            <span className="font-mono">{span.tokens_per_second.toFixed(1)} tok/s</span>
+                          </div>
+                        )}
+                      </div>
+                    )}
+
                     {/* Error Message */}
                     {currentSpan.error_message !== null && (
                       <div className="space-y-2">
