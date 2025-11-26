cbce2597b111e856d122245034a7834187be2b7c
Siddharth Balaji <siddharthbalaji6@gmail.com>
2025-11-26 01:16:50 -0800

    refactor: made changes to prompt versions db and spans db to account for new prompt versions

--- database/init.sql
+++ database/init.sql
@@ -48,7 +48,11 @@ CREATE TABLE spans (
     error_message VARCHAR(200),
     is_streaming BOOLEAN DEFAULT FALSE,
     time_to_first_token FLOAT,
-    tokens_per_second FLOAT
+    tokens_per_second FLOATm
+    prompt_id UUID REFERENCES prompt_versions(prompt_version_id),
+    prompt_version TEXT,
+    prompt_hash TEXT
+
 );
 
 CREATE TABLE prompt_versions (
@@ -57,7 +61,12 @@ CREATE TABLE prompt_versions (
     version_number INT,
     s3_url VARCHAR(200),
     created_at TIMESTAMP,
-    is_active BOOLEAN
+    is_active BOOLEAN,
+    agent_id UUID REFERENCES agents(agent_id),
+    prompt_hash TEXT,
+    content_preview TEXT,
+    metadata JSONB,
+    parent_version_id UUID
 );
 
 CREATE TABLE evaluations (
@@ -75,3 +84,5 @@ CREATE INDEX idx_spans_status ON spans(status);
 CREATE INDEX idx_prompt_versions_version_number ON prompt_versions(version_number);
 CREATE INDEX idx_evaluations_trace_id ON evaluations(trace_id);
 CREATE UNIQUE INDEX idx_agents_api_key ON agents(api_key);
+CREATE INDEX prompts_per_agent ON prompt_versions(agent_id, name);
+CREATE INDEX prompt_analytics ON spans(prompt_id, prompt_version);
