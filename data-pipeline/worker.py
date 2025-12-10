68de415fe97333c9cdefe4f74e88bb4e8d2f6f2f
Siddharth Balaji <siddharthbalaji6@gmail.com>
2025-12-10 14:44:50 -0800

    feat: finalize trace now calls trace updating method

--- data-pipeline/worker.py
+++ data-pipeline/worker.py
@@ -518,6 +518,7 @@ class SpanWorker:
                 "total_cost": float(total_cost or 0)
             }
             db.update_trace(trace_id, update_data)
+            self.publish_trace_completed(trace_id, trace.get('user_id'), update_data)
             
             event = {
                 "type": "trace_completed",
